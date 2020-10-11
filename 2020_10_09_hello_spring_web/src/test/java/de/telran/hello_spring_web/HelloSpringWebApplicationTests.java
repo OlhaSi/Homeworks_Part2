7236860a7ca46d13257dba38de1c11207512fe53
OlhaSi <liush.olga@gmail.com>
2020-10-11 23:46:19 +0200

    Add Spring Web realisation

---
+++ 2020_10_09_hello_spring_web/src/test/java/de/telran/hello_spring_web/HelloSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.hello_spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
